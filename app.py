6dd7b0e6c066d2745a6ee8efb5324b1648f4c4f4
btbr1216 <batun@nycstudents.net>
2026-09-15 06:20:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,43 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+# t.forward(200)
 
-turtle.done()
+# turtle.done()
+
+def double(one, two):
+    print(one)
+    print(two)
+double(6, 7)
+
+def turn(number, shapee, side):
+
+    turnamt = 0
+    if shapee == "square":
+        turnamt = 90
+    elif shapee == "triangle":
+        turnamt = 120
+    
+    if side == "left":
+        t.forward(-number)
+        t.left(-turnamt)
+    elif True:
+        t.forward(number)
+        t.left(turnamt)
+
+
+
+shapee = "triangle"
+size = 500
+side = "left"
+
+
+
+rangee = 0
+if shapee == "square":
+    rangee = 4
+elif shapee == "triangle":
+    rangee = 3
+
+for i in range(rangee):
+    turn(size, shapee, side)
